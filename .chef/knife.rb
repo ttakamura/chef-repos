6bb2a377321346d5816d74a3f0cb769baa60af8f
Tatsuya Takamura <takamura@tonchidot.com>
2014-04-12 16:00:34 +0900

    Init

---
+++ .chef/knife.rb
@@ -0,0 +1,9 @@
+log_level     :info
+log_location  STDOUT
+cookbook_path ["cookbooks", "site-cookbooks"]
+node_path     "nodes"
+role_path     "roles"
+data_bag_path "data_bags"
+#encrypted_data_bag_secret "data_bag_key"
+
+knife[:berkshelf_path] = "cookbooks"
---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://tokyo-m.rubygems.org'
+
+gem 'chef'
+gem 'knife-solo'
+gem 'berkshelf'
