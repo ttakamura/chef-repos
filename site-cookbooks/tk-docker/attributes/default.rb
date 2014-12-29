6574929f0201e0a696b2e47056f13b1f8a9e0801
Tatsuya Takamura <takamura@tonchidot.com>
2014-12-30 08:20:49 +0900

    update

--- site-cookbooks/tk-docker/attributes/default.rb
+++ site-cookbooks/tk-docker/attributes/default.rb
@@ -1,7 +1,10 @@
-default['docker']['bind_uri']       = 'tcp://0.0.0.0:4243'
+
+# default['docker']['exec_driver']    = 'lxc'
+default['docker']['host']           = ['unix:///var/run/docker.sock', 'tcp://0.0.0.0:4243']
 default['docker']['options']        = '-g=/mnt/docker'
-default['docker']['exec_driver']    = 'lxc'
 default['docker']['storage_driver'] = 'aufs'
+default['docker']['version']        = '1.4.1'
+
 default['dokku']['version']         = '0.2.2'
 default['dokku']['plugins']         = {
   'docker-options' => 'https://github.com/dyson/dokku-docker-options.git',
